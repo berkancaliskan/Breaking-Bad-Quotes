db8715bcb567f64d87cb55928a8c513d968eb778
Berkan Çalışkan <45601088+berkancaliskan@users.noreply.github.com>
2026-01-27 17:59:14 +0300

    project created, appicon and background images are added

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Berkan Çalışkan on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
